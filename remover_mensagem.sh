fca28439663f99cc400012b5460465d2367cdbae
ro-ferreira94 <rodrigo.ferreira@rodrigoferreira.local>
2021-08-03 15:16:54 -0300

    Modificacao

--- remover_mensagem.sh
+++ remover_mensagem.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 mysql -e "show databases;" | grep -i mboxgroup > /tmp/database
 
-echo 'Assunto da mensagem ?'
+echo 'Digite um texto no corpo da mensagem :'
 read ASS
 
-RESL=`for i in $(cat /tmp/database) ; do mysql -b "$i" -e "select * from mail_item where subject '%$ASS%' ;" ; done > /tmp/mensagem`
+RESL=`for i in $(cat /tmp/database) ; do mysql -b "$i" -e "select * from mail_item where metadata like '%$ASS%' ;" ; done > /tmp/mensagem`
 QT=`cat /tmp/mensagem | wc -l`
 echo -e "\n FORAM ENCONTRADOS $QT MENSAGENS COM O ASSUNTO $ASS \n"
 
@@ -12,7 +12,7 @@ read -p " DESEJA EXCLUIR ESTAS MENSAGENS ? ( responda sim ou nao ) : " ASK
 
 if [ "$ASK" == "sim" ]
 then
-  for i in $(cat /tmp/database) ; do mysql -b "$i" -e "delete from mail_item where subject '%$ASS%' ;" ; done
+  for i in $(cat /tmp/database) ; do mysql -b "$i" -e "delete from mail_item where metadata like '%$ASS%' ;" ; done
   echo -e "\nTODAS AS MENSAGENS COM ESTE ASSUNTO $ASS FORAM EXCLUIDAS\n"
 fi
 
